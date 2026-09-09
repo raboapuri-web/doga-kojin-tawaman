fb1a2c9b07e5986500350cabab29e407ff5afdd7
raboapuri-web <raboapuri@gmail.com>
2026-09-09 23:00:23 +0900

    Switch V18 root to type-safe cinematic implementation

--- v18-bubble-cinematic/src/Root.tsx
+++ v18-bubble-cinematic/src/Root.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {V18BubbleCinematic} from './V18BubbleCinematic';
+import {V18BubbleCinematicFixed} from './V18BubbleCinematicFixed';
 import {getRenderDurationSeconds} from './timing';
 
 export const RemotionRoot:React.FC=()=>(
   <Composition
     id="V18BubbleCinematic"
-    component={V18BubbleCinematic}
+    component={V18BubbleCinematicFixed}
     durationInFrames={Math.ceil(getRenderDurationSeconds()*30)}
     fps={30}
     width={1920}
