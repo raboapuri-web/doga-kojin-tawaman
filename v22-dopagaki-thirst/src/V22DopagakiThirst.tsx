773d5390b18d970413ceb9cc24fed91ee077565c
raboapuri-web <raboapuri@gmail.com>
2026-09-10 21:53:00 +0900

    Add V22 main Remotion renderer

---
+++ v22-dopagaki-thirst/src/V22DopagakiThirst.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useCurrentFrame,useVideoConfig} from 'remotion';
+import scriptData from './script-data.json';
+import {getActiveBeatAtSeconds} from './timing';
+import {CaptionLayer,Visual,type Beat} from './scenes';
+
+const beats=scriptData.beats as Beat[];
+
+export const V22DopagakiThirst:React.FC=()=>{
+  const frame=useCurrentFrame();
+  const {fps}=useVideoConfig();
+  const active=getActiveBeatAtSeconds(frame/fps);
+  const beat=beats[active.index]??beats[0];
+  return <><Visual beat={beat} p={active.progress}/><CaptionLayer beat={beat} p={active.progress}/></>;
+};
