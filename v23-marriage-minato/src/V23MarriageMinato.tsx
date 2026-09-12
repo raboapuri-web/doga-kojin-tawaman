40068ca0e22be9f57d6634b30e3d1c601542d243
raboapuri-web <raboapuri@gmail.com>
2026-09-12 14:48:09 +0900

    Add V23 marriage minato composition

---
+++ v23-marriage-minato/src/V23MarriageMinato.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useCurrentFrame,useVideoConfig} from 'remotion';
+import beatsData from './beats.json';
+import {getActiveBeat} from './timing';
+import {Caption,Visual,type Beat} from './scenes';
+
+const beats=beatsData as Beat[];
+export const V23MarriageMinato:React.FC=()=>{
+  const frame=useCurrentFrame();
+  const {fps}=useVideoConfig();
+  const active=getActiveBeat(frame/fps);
+  const beat=beats[active.index]??beats[0];
+  return <><Visual beat={beat} p={active.progress}/><Caption beat={beat} p={active.progress}/></>;
+};
