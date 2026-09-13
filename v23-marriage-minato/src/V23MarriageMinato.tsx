af4ca22ef05b1d92a07a17bfaa868e110d4fd426
raboapuri-web <raboapuri@gmail.com>
2026-09-14 00:31:06 +0900

    Switch Tokyo envy video to rich multi-shot scene renderer

--- v23-marriage-minato/src/V23MarriageMinato.tsx
+++ v23-marriage-minato/src/V23MarriageMinato.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useCurrentFrame,useVideoConfig} from 'remotion';
 import beatsData from './beats.json';
 import {getActiveBeat} from './timing';
-import {Caption,Visual,type Beat} from './scenes';
+import {Caption,Visual,type Beat} from './rich-scenes';
 
 const beats=beatsData as Beat[];
 export const V23MarriageMinato:React.FC=()=>{
