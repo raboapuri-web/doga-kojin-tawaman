ee444ad1d7aacd6dcf9defed824ac4715095ace3
raboapuri-web <raboapuri@gmail.com>
2026-09-14 14:58:00 +0900

    Switch R3F demo to first-minute composition

--- r3f-demo/src/Root.tsx
+++ r3f-demo/src/Root.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {FriendsDemo} from './FriendsDemo';
+import {FriendsFirstMinute} from './FriendsFirstMinute';
 
 export const Root: React.FC = () => (
   <>
     <Composition
-      id="R3FFriendsDemo"
-      component={FriendsDemo}
+      id="R3FFriendsFirstMinute"
+      component={FriendsFirstMinute}
       width={1920}
       height={1080}
       fps={30}
-      durationInFrames={360}
+      durationInFrames={1800}
     />
   </>
 );
